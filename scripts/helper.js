545af4222d3e0f0cd5c74b5cfbf743c786b0f97a
Linda O'Neill <oneill.linda21@gmail.com>
2018-01-22 16:05:09 -0600

    update index.html

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,3 +1,14 @@
+//create a class called Helper, instantiate as a variable called helper
 class Helper {
-  
+  constructor () {
+//add method called playPauseAndUpdate that takes one parameter, song
+helper.playPauseAndUpdate(song) = player.playPause(song);
+
+//call player.playPause, pass song parameter, update total time
+$('#total-time input').on('input', function (event) {
+  player.playPause(song);
+});
+
+//replace every call of player.playPause with helper.playPauseAndUpdate
+player.playPause = helper.playPauseAndUpdate
   }
